c7f0b3f251cd74ac4aca112576180c000bbbd405
serkankose <kose.serkan@gmail.com>
2022-02-26 16:01:08 +0000

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className="calculator">
+                <div>
+                    <Button variant="contained" color="primary">1</Button>
+                    <Button variant="contained" color="primary">2</Button>
+                    <Button variant="contained" color="primary">3</Button>
+                    <Button variant="contained" color="primary">+</Button>
+                    <Button variant="contained" color="primary">*</Button>
+                </div>
+                <div>
+                    <Button variant="contained" color="primary">4</Button>
+                    <Button variant="contained" color="primary">5</Button>
+                    <Button variant="contained" color="primary">6</Button>
+                    <Button variant="contained" color="primary">-</Button>
+                    <Button variant="contained" color="primary">/</Button>
+                </div>
+                <div>
+                    <Button variant="contained" color="primary">7</Button>
+                    <Button variant="contained" color="primary">8</Button>
+                    <Button variant="contained" color="primary">9</Button>
+                    <Button variant="contained" color="primary">^</Button>
+                    <Button variant="contained" color="primary">=</Button>
+                </div>
+            </div>
+            <div className="history">
+                <ul>
+                    <li>1 + 2</li>
+                </ul>
+            </div>
+
+        </div>
+
+    );
 }
 
 export default App;
